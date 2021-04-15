a5155abec9ab3f71aac17d53cf9a0a859e9a77d8
Ricky Marasigan <rickymarasigan@Rickys-MacBook-Pro.local>
2021-04-15 11:21:01 -0700

    completed youtube tutorial

---
+++ src/components/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Profile = () => {
+    const { user, isAuthenticated } = useAuth0();
+
+    return (
+        isAuthenticated && (
+            <>
+                <h1>{user.name}</h1>
+                <img src={user.picture} alt={user.name}></img>
+                <p>{user.email}</p>
+            </>
+        )
+    )
+}
+
+export default Profile;
