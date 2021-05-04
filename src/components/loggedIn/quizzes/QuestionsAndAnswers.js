7206ccfbb010d55a1ce2fee8b68b468d408cd0f6
Ricky Marasigan <rickymarasigan@Rickys-MacBook-Pro.local>
2021-05-04 16:30:25 -0700

    moved quiz questions and answers to their own separate file

---
+++ src/components/loggedIn/quizzes/QuestionsAndAnswers.js
@@ -0,0 +1,33 @@
+module.exports = {
+    'Note Names': [
+        'C', 'D♭', 'D', 'E♭', 'E', 'F', 'G♭', 'G', 'A♭', 'A', 'B♭', 'B'
+    ],
+    'Chords': [
+        { question: 'C', answer: ['C', 'E', 'G'] },
+        { question: 'D♭', answer: ['D♭', 'F', 'A♭'] },
+        { question: 'D', answer: ['D', 'G♭', 'A'] },
+        { question: 'E♭', answer: ['E♭', 'G', 'B♭'] },
+        { question: 'E', answer: ['E', 'A♭', 'B'] },
+        { question: 'F', answer: ['F', 'A', 'C'] },
+        { question: 'G♭', answer: ['G♭', 'B♭', 'D♭'] },
+        { question: 'G', answer: ['G', 'B', 'D'] },
+        { question: 'A♭', answer: ['A♭', 'C', 'E♭'] },
+        { question: 'A', answer: ['A', 'D♭', 'E'] },
+        { question: 'B♭', answer: ['B♭', 'D', 'F'] },
+        { question: 'B', answer: ['B', 'E♭', 'G♭'] }
+    ],
+    'Intervals': [
+        { question: 'half step', answer: 'D♭' },
+        { question: 'full step', answer: 'D' },
+        { question: 'minor third', answer: 'E♭' },
+        { question: 'major third', answer: 'E' },
+        { question: 'perfect fourth', answer: 'F' },
+        { question: 'tritone', answer: 'G♭' },
+        { question: 'perfect fifth', answer: 'G' },
+        { question: 'minor sixth', answer: 'A♭' },
+        { question: 'major sixth', answer: 'A' },
+        { question: 'minor seventh', answer: 'B♭' },
+        { question: 'major seventh', answer: 'B' },
+        { question: 'octave', answer: 'C' }
+    ]
+};
--- src/components/loggedIn/quizzes/Quiz.js
+++ src/components/loggedIn/quizzes/Quiz.js
@@ -1,44 +1,13 @@
 import React, { Component } from 'react';
-import Piano from '../Piano'
+import Piano from '../Piano';
+import QuestionsAndAnswers from './QuestionsAndAnswers';
 
 class Quiz extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            questions: {
-                'Note Names': [
-                    'C', 'D♭', 'D', 'E♭', 'E', 'F', 'G♭', 'G', 'A♭', 'A', 'B♭', 'B'
-                ],
-                'Chords': [
-                    { question: 'C', answer: ['C', 'E', 'G'] },
-                    { question: 'D♭', answer: ['D♭', 'F', 'A♭'] },
-                    { question: 'D', answer: ['D', 'G♭', 'A'] },
-                    { question: 'E♭', answer: ['E♭', 'G', 'B♭'] },
-                    { question: 'E', answer: ['E', 'A♭', 'B'] },
-                    { question: 'F', answer: ['F', 'A', 'C'] },
-                    { question: 'G♭', answer: ['G♭', 'B♭', 'D♭'] },
-                    { question: 'G', answer: ['G', 'B', 'D'] },
-                    { question: 'A♭', answer: ['A♭', 'C', 'E♭'] },
-                    { question: 'A', answer: ['A', 'D♭', 'E'] },
-                    { question: 'B♭', answer: ['B♭', 'D', 'F'] },
-                    { question: 'B', answer: ['B', 'E♭', 'G♭'] }
-                ],
-                'Intervals': [
-                    { question: 'half step', answer: 'D♭' },
-                    { question: 'full step', answer: 'D' },
-                    { question: 'minor third',answer: 'E♭' },
-                    { question: 'major third',answer: 'E' },
-                    { question: 'perfect fourth',answer: 'F' },
-                    { question: 'tritone',answer: 'G♭' },
-                    { question: 'perfect fifth',answer: 'G' },
-                    { question: 'minor sixth',answer: 'A♭' },
-                    { question: 'major sixth',answer: 'A' },
-                    { question: 'minor seventh',answer: 'B♭' },
-                    { question: 'major seventh',answer: 'B' },
-                    { question: 'octave',answer: 'C' }
-                ]
-            },
+            questions: QuestionsAndAnswers,
             noteLimit: { 'Note Names': 1, 'Chords': 3, 'Intervals': 1 },
             answers: [],
             pendingAnswer: this.props.quizType === 'Chords' ? [] : null,
